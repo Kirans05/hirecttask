8aba3eaaa228645d89c606509bc66a0c9f33d940
kiran s <kirans08298@gmail.com>
2022-04-20 18:09:36 +0530

    second commit

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const jwt = require("jsonwebtoken")
 const cookieParser = require("cookie-parser")
 const corsOptions = {
     credentials: true,
-    ///..other options
   };
 app.use(cookieParser())
 app.use(cors(corsOptions));
 const {haspPassword,comparePassword} = require("./hashPage")
+const port = process.env.PORT || 8000
 
 
 app.post("/login/signin",async (req,res)=>{
@@ -35,17 +35,17 @@ app.post("/login/signin",async (req,res)=>{
         let table = await db.collection("usersTable")
         let user = await table.find({email}).toArray()
         if(user.length){
-            const logintocken = jwt.sign({_id:user[0]._id},process.env.secret_key,{expiresIn:"1m"})
+            const logintocken = jwt.sign({_id:user[0]._id,email:user[0].email},process.env.secret_key,{expiresIn:"7d"})
             const upadteTocken = await table.updateOne({email:user[0].email},{$set:{tocken:logintocken}})
             const {_id,name,email} = user[0]
             res.status(200).send({logintocken,user:{_id,name,email},message:"Login Successfull"})
         }else{
-            const SigninTocken = jwt.sign({name,email},process.env.secret_key,{expiresIn:"1m"})
+            const tocken = jwt.sign({name,email},process.env.secret_key,{expiresIn:"7d"})
             let pwd = name+email
             let newPassword = await haspPassword(pwd)
             password = newPassword
-            let newUser = await table.insertOne({name,email,password,SigninTocken})
-            res.status(200).send({SigninTocken,user:{name,email},message:"SignUp successfull"})
+            let newUser = await table.insertOne({name,email,password,tocken})
+            res.status(200).send({tocken,user:{name,email},message:"SignUp successfull"})
         }
     }
     }catch(error) {
@@ -57,4 +57,50 @@ app.post("/login/signin",async (req,res)=>{
 
 
 
-app.listen(8000)
+// inserting search keyword
+
+app.post("/searchKeyword",async (req,res) => {
+    console.log(req.body)
+        try{
+            let resposne = await backend.connect()
+            let db = await resposne.db(databasename)
+            let table = await db.collection("usersTable")
+            let user = await table.find({tocken:req.body.tocken}).toArray()
+            console.log("===>",user)
+            if(user.length){
+                if(user[0].searchKeywords){
+                    let value = req.body.keys 
+                    user[0].searchKeywords.push(value)
+                    let insertNewKeys = await table.updateOne({email:user[0].email},{$set:{searchKeywords:user[0].searchKeywords}})
+                    res.json({message:"New Keys Inserted"})
+                }else{
+                    let arr = [req.body.keys]
+                    let insertKeys = await table.updateOne({email:user[0].email},{$set:{searchKeywords:arr}})
+                    res.json({message:"Keys Inserted for First time"})
+                }
+            }else{
+                res.json({message:"User Not Found"})
+            }
+        }catch(error) {
+            res.json({message:"Internal Server Problem"})
+        }
+})
+
+
+
+// display searched key words
+// app.post("/displaySearch",async (req,res) => {
+//     console.log(req.body)
+//     let resposne = await backend.connect()
+//     let db = await resposne.db(databasename)
+//     let table = await db.collection("usersTable")
+//     let user = await table.find({tocken:req.body.tocken}).toArray()
+//     res.json({
+//         keys:user[0].searchKeywords
+//     })
+// })
+
+
+
+
+app.listen(port)
